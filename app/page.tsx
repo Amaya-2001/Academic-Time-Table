7c5bd16ca91c136dc9719028de74232c258a428f
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-07-10 09:48:19 +0530

    set up next js environment and installed tailwind and daisy UI CSS

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+    <h1 className="text-center underline">Next Js</h1>
+    <button className='btn'>click me</button>
+   
+    </main>
+  )
+}
